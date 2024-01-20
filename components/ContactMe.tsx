d41e840f598411cc0353410e29a2700943e7174d
EricKisee <erickiseemulwa@gmail.com>
2024-01-20 23:20:58 +0300

    Remove scroll on contact me

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -28,7 +28,7 @@ export default function ContactMe({}: Props) {
     <div className=' my-10 h-screen w-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className=' absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '> Contact</h3>
 
-        <div className='flex flex-col space-y-10 h-21 scrollbar-thin overflow-y-scroll'>
+        <div className='flex flex-col space-y-10 h-21'>
             <h4 className=' text-xl sm:text-xl md:text-2xl font-semibold text-center'>Share some feedback. {' '} <span className=' decoration-[#F7AB0A]/50 underline'>Lets Talk.</span></h4> 
             <div className='space-y-10'>
                 <div className=' flex items-center space-x-5 justify-center'>
