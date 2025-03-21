6e9a660f6d6f6371e07be6e482762e7c44a3f680
EricKisee <erickiseemulwa@gmail.com>
2025-03-21 12:14:59 +0300

    fixes - skills images visibility on mobile devices

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -22,9 +22,14 @@ export default function Skill({skill, directionLeft}: Props) {
         whileInView={{ opacity:1, x:0}}
         src={urlFor(skill.image).url()}
         alt=''
-        className='rounded-full border border-gray-500 object-cover w-10 h-10 sm:w-24 sm:h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='sm:flex hidden rounded-full border border-gray-500 object-cover w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
-        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-10 h-10 sm:w-24 sm:h-24 md:w-28 md:h-28  rounded-full z-0 '>
+        <img
+        src={urlFor(skill.image).url()}
+        alt=''
+        className='flex sm:hidden rounded-full border border-gray-500 object-cover w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28  rounded-full z-0 '>
             <div className='flex items-center justify-center h-full'> 
                 <p className='text-sm sm:text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
