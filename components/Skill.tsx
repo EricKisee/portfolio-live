3b71cf04aa04f39557a67342be12d43f47a659a3
EricKisee <erickiseemulwa@gmail.com>
2025-03-21 11:14:42 +0300

    fixes - skills images visibility on mobile devices

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -22,9 +22,9 @@ export default function Skill({skill, directionLeft}: Props) {
         whileInView={{ opacity:1, x:0}}
         src={urlFor(skill.image).url()}
         alt=''
-        className='rounded-full border border-gray-500 object-cover w-11 h-11 sm:w-24 sm:h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
-        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-11 h-11 sm:w-24 sm:h-24 md:w-28 md:h-28  rounded-full z-0 '>
+        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28  rounded-full z-0 '>
             <div className='flex items-center justify-center h-full'> 
                 <p className='text-sm sm:text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
