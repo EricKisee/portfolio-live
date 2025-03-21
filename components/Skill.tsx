a8bf2106b8ec41345518b5b057a65a44bb9d82a1
EricKisee <erickiseemulwa@gmail.com>
2025-03-21 11:10:42 +0300

    fixes - skills images visibility on mobile devices

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import {motion} from 'framer-motion'
-import { Skill } from '@/typings'
+import type { Skill } from '@/typings'
 import { urlFor } from '@/sanity'
 
 
@@ -22,11 +22,11 @@ export default function Skill({skill, directionLeft}: Props) {
         whileInView={{ opacity:1, x:0}}
         src={urlFor(skill.image).url()}
         alt=''
-        className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-11 h-11 sm:w-24 sm:h-24 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
-        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-24 h-24 md:w-28 md:h-28  rounded-full z-0 '>
+        <div className=' absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-11 h-11 sm:w-24 sm:h-24 md:w-28 md:h-28  rounded-full z-0 '>
             <div className='flex items-center justify-center h-full'> 
-                <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
+                <p className='text-sm sm:text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
         </div>
     </div>
