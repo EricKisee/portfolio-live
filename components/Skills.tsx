2dfe5ab5ded93e5f9a1c4afa50cf78710a3449f5
EricKisee <erickiseemulwa@gmail.com>
2025-03-21 11:01:01 +0300

    fixes - skills images visibility on mobile devices

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -19,7 +19,18 @@ export default function Skills({skills}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'> Skills</h3>
         <h3 className=' absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'> Hover over a skill for current profieciency</h3>
 
-        <div className='grid grid-cols-5 gap-5'>
+        <div className=' hidden sm:grid  grid-cols-5 gap-5'>
+          {skills?.slice(0,skills.length/2).map(skill=>(
+            <Skill key={skill._id} skill={skill} />
+          ))}
+
+          {skills?.slice(skills.length/2,skills.length).map(skill=>(
+            <Skill key={skill._id} skill={skill} directionLeft />
+          ))}
+
+
+        </div>
+        <div className=' grid sm:hidden  grid-cols-4 gap-2'>
           {skills?.slice(0,skills.length/2).map(skill=>(
             <Skill key={skill._id} skill={skill} />
           ))}
