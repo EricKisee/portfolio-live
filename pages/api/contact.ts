ce0903029774eb2c75b16c5749371659e3d59271
EricKisee <erickiseemulwa@gmail.com>
2025-05-13 12:42:56 +0300

    Contact form Rework -  custom email  and domain  setup

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -3,10 +3,15 @@ import { formSchema } from "@/utils/schema";
 import { EmailTemplate } from "@/components/email-template";
 import { Resend } from "resend";
 import { sanityClient } from "@/sanity";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
